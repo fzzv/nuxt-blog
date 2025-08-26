a898034266fe1c1743bce971bebe1329c64bd3df
fan <291950304@qq.com>
2025-08-26 16:30:28 +0800

    chore: update text color in dark mode

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    <div relative ref="heightRef" dark:text-gray-100 >
+  <div dark:text-gray-100>
+    <div relative ref="heightRef">
       <div v-motion-pop-visible fc flex-col>
         <img w-40 h-40 border-rounded-40 transition-all duration-500 ease-in-out src="/images/me.png" alt="" />
         <h1 font-400 text-2xl md:text-3xl mt-5>Fan</h1>
         <span text-base md:text-xl>Front-end Developer</span>
-        <div fcc mt-3 text-base leading-5 dark:text-gray-100>
+        <div fcc mt-3 text-base leading-5>
           <a v-for="route in routes" :href="route.href" target="_blank" ml-5>
             <span text-5 icon-btn :class="route.icon"></span>
           </a>
@@ -16,7 +16,7 @@
     <div w-full text-center mt-10 mx-auto my-0>
         你好！我是一名前端工程师，这个博客主要记录了一些学习成长笔记。
       </div>
-    <div mt-7 text-center dark:text-gray-100>
+    <div mt-7 text-center>
       <h1>我的技术栈</h1>
       <div w-full max-w-169 mt-10 mx-auto flex flex-wrap>
         <span v-for="item in skills" w-20 h-5 px-4 py-2 mx-5 my-5 border-2 rounded-2xl shadow-lg shadow-gray-900 dark:shadow-gray-300>

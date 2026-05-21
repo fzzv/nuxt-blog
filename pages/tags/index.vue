8df9c1319e146bb7f979e2ce439f48be37ce17f8
fan <291950304@qq.com>
2026-05-21 10:06:36 +0800

    chore: optimize tags item style

--- pages/tags/index.vue
+++ pages/tags/index.vue
@@ -1,10 +1,15 @@
 <template>
-  <div font-mono dark:text-gray-300 >
+  <div font-mono dark:text-gray-300>
     <h1 text-center>标签</h1>
-    <div mt-10 flex flex-wrap>
-      <span v-for="item in sortedTags" v-motion-pop h-4 px-4 py-2 mx-5 my-5 border-2 rounded-2xl shadow-lg shadow-gray-900 dark:shadow-gray-300>
-        <NuxtLink :to="`/tags/${item}`" capitalize font-bold><span text-blue-600 >
-          {{ item }}</span>&nbsp;({{ tagCount[item] }})
+    <div mt-10 flex flex-wrap justify-center gap-5>
+      <span
+        v-for="item in sortedTags"
+        :key="item"
+        v-motion-pop
+        inline-flex w-fit shrink-0 items-center rounded-2xl border-2 px-4 py-2 shadow-lg shadow-gray-900 dark:shadow-gray-300
+      >
+        <NuxtLink :to="`/tags/${item}`" capitalize whitespace-nowrap font-bold>
+          <span text-blue-600>{{ item }}</span>&nbsp;({{ tagCount[item] }})
         </NuxtLink>
       </span>
     </div>
@@ -12,25 +17,29 @@
 </template>
 
 <script lang="ts" setup>
-interface TagObj {
-  [key: string]: number;
+interface TagDoc {
+  tags?: string[]
 }
+
+type TagCountMap = Record<string, number>
+
 // 获取所有文章的标签
-const { data } = await useAsyncData("tags", () => queryContent("posts").only(["tags"]).find())
-// 用于收集标签的个数
-const tagCount = ref<TagObj>({})
-const tagData = unref(data)
-if (Array.isArray(tagData)) {
-  tagData.forEach((item) => {
-    item.tags.forEach((tag: string) => {
-      if (tag in tagCount.value) {
-        tagCount.value[tag] += 1
-      } else {
-        tagCount.value[tag] = 1
-      }
+const { data } = await useAsyncData<TagDoc[]>('tags', () =>
+  queryContent('posts').only(['tags']).find(),
+)
+
+// 统计每个标签的数量
+const tagCount = computed<TagCountMap>(() => {
+  return (data.value ?? []).reduce<TagCountMap>((countMap, { tags = [] }) => {
+    tags.forEach((tag) => {
+      countMap[tag] = (countMap[tag] ?? 0) + 1
     })
-  })
-}
+    return countMap
+  }, {})
+})
+
 // 标签排序
-const sortedTags = Object.keys(tagCount.value).sort((a, b) => tagCount.value[b] - tagCount.value[a])
+const sortedTags = computed(() => {
+  return Object.keys(tagCount.value).sort((a, b) => tagCount.value[b] - tagCount.value[a])
+})
 </script>
